176ba3e5e1b9f97d99fd56d19ff6135dfdc16ee5
Dirk Baeumer <dirkb@microsoft.com>
2022-11-16 13:20:31 +0100

    Use esbuild to compile TS

--- bin/esbuild.js
+++ bin/esbuild.js
@@ -6,44 +6,69 @@
 
 const esbuild = require('esbuild');
 
-const webExtension = esbuild.build({
-	entryPoints: ['out/web/extension.js'],
-	outfile: 'dist/web/extension.js',
+let watch;
+if (process.argv.includes('--watch')) {
+	watch = {
+		onRebuild(error, result) {
+			if (error) {
+				console.error('watch build failed:', error);
+			} else {
+				console.log('watch build succeeded');
+			}
+		}
+	};
+}
+
+/**
+ * @typedef {import('esbuild').BuildOptions} BuildOptions
+ */
+
+/** @type BuildOptions */
+const sharedBrowserOptions = {
 	bundle: true,
 	external: ['vscode'],
-	format: 'cjs',
 	target: 'es2020',
-	platform: 'browser'
+	platform: 'browser',
+	sourcemap: true,
+	watch
+};
+
+const webExtension = esbuild.build({
+	entryPoints: ['src/web/extension.ts'],
+	outfile: 'dist/web/extension.js',
+	format: 'cjs',
+	...sharedBrowserOptions,
 }).catch(console.error);
 
 const webWorker = esbuild.build({
-	entryPoints: ['out/web/pythonWasmWorker.js'],
+	entryPoints: ['src/web/pythonWasmWorker.ts'],
 	outfile: 'dist/web/pythonWasmWorker.js',
-	bundle: true,
-	external: ['vscode'],
 	format: 'iife',
-	target: 'es2020',
-	platform: 'browser'
+	...sharedBrowserOptions,
 }).catch(console.error);
 
-const desktopExtension = esbuild.build({
-	entryPoints: ['out/desktop/extension.js'],
-	outfile: 'dist/desktop/extension.js',
+/** @type BuildOptions */
+const sharedDesktopOptions = {
 	bundle: true,
 	external: ['vscode'],
-	format: 'cjs',
 	target: 'es2020',
-	platform: 'node'
+	platform: 'node',
+	sourcemap: true,
+	watch
+};
+
+const desktopExtension = esbuild.build({
+	entryPoints: ['src/desktop/extension.ts'],
+	outfile: 'dist/desktop/extension.js',
+	format: 'cjs',
+	...sharedDesktopOptions,
 }).catch(console.error);
 
 const desktopWorker = esbuild.build({
-	entryPoints: ['out/desktop/pythonWasmWorker.js'],
+	entryPoints: ['src/desktop/pythonWasmWorker.ts'],
 	outfile: 'dist/desktop/pythonWasmWorker.js',
-	bundle: true,
-	external: ['vscode'],
 	format: 'iife',
-	target: 'es2020',
-	platform: 'node'
+	...sharedDesktopOptions,
 }).catch(console.error);
 
 
