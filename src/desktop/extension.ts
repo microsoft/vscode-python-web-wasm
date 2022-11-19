ba7ff84db1671dc47ad2c62c8c7b50914fcb7835
Rich Chiodo <rchiodo@users.noreply.github.com>
2022-11-18 17:43:19 -0800

    Put back unnecessary changes

--- src/desktop/extension.ts
+++ src/desktop/extension.ts
@@ -5,22 +5,7 @@
 import RIL from './ril';
 RIL.install();
 
-import { activate as commonActivate, deactivate as _deactivate } from '../common/extension';
-import { debug, DebugAdapterDescriptor, DebugAdapterInlineImplementation, DebugSession, ExtensionContext } from 'vscode';
-import { DebugAdapter } from '../common/debugAdapter';
-import RAL from '../common/ral';
+import { activate as _activate, deactivate as _deactivate } from '../common/extension';
 
-class DebugAdapterDescriptorFactory implements DebugAdapterDescriptorFactory {
-	constructor(private readonly context: ExtensionContext, private readonly preloadPromise: Promise<void>) {
-	}
-	async createDebugAdapterDescriptor(session: DebugSession): Promise<DebugAdapterDescriptor> {
-		await this.preloadPromise;
-		return new DebugAdapterInlineImplementation(new DebugAdapter(session, this.context, RAL()));
-	}
-}
-
-
-export function activate(context: ExtensionContext) {
-	return commonActivate(context);
-}
+export const activate = _activate;
 export const deactivate = _deactivate;
